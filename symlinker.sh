f5c9a72feab3bd07346a8e9db787abcf42a7467f
Carlo Federico Pauletti <cpaulett@sissa.it>
2022-10-16 17:39:28 +0200

    added symlinker

---
+++ symlinker.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# my silly way of managing dotfiles when porting my rice to a new installation:
+# simply run the script to automatically symlink everything in the dotfiles
+# repo to the right location.
+
+echo "Enter username: "
+read USER
+
+[[ -f "/home/$USER/.local" ]] || mkdir /home/$USER/.local 2>&1 >/dev/null
+[[ -f "/home/$USER/.local/bin" ]] || mkdir /home/$USER/.local/bin 2>&1 >/dev/null
+[[ -f "/home/$USER/.local/bin/statusbar" ]] || mkdir /home/$USER/.local/bin/statusbar 2>&1 >/dev/null
+
+# statusbar
+#for script in /home/$USER/.programs/scripts/statusbar
+#do
+#    ln -s /home/$USER/.programs/scripts/statusbar/$script /home/$USER/.local/bin/statusbar/$script
+#done
+
+# statusbar-minimal
+for script in /home/$USER/.programs/scripts/statusbar-minimal
+do
+    ln -s /home/$USER/.programs/scripts/statusbar-minimal/$script /home/$USER/.local/bin/statusbar/$script
+done
+
+ln -s /home/$USER/.programs/scripts/dmenu_run_i /home/$USER/.local/bin/scripts/dmenu_run_i
